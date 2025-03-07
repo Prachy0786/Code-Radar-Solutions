c949dc00653f0e096790ef3f7fda730735bc7949
Prachy0786 <194909019+Prachy0786@users.noreply.github.com>
2025-03-07 22:38:21 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,12 @@
+// Your code here...
+void fibonacciSeries(int n)
+{
+    int i,a=0,b=1,c=0;
+    for(i=1;i<=5;i=i+1)
+    {
+        printf("%d",a);
+        c=a+b;
+        b=a;
+        a=c;
+    }
+}
