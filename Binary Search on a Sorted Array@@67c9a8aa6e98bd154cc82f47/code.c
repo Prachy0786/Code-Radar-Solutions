5d89e7abba422221fbee353532a54bc97c6bf569
Prachy0786 <194909019+Prachy0786@users.noreply.github.com>
2025-03-12 13:11:04 +0530

    Solution to Binary Search on a Sorted Array

---
+++ Binary Search on a Sorted Array@@67c9a8aa6e98bd154cc82f47/code.c
@@ -0,0 +1,13 @@
+// Your code here...
+int binarySearch(int* a,int n,int t)
+{
+    int i,c=-1;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+            c=i;
+        }
+    }
+    return c;
+}
