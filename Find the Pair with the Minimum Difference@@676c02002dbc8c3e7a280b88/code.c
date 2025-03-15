ddb231f59208add72a275f16d4835540d4976305
Prachy0786 <194909019+Prachy0786@users.noreply.github.com>
2025-03-15 11:56:45 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -3,42 +3,33 @@
 int main()
 {
     int n;     scanf("%d",&n);
-    int i,a[n],c=1000,d=0,e=0;
+    int i,a[n],c=1000,j=0,e=0;
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
-    if(n==1)
+    int d;
+    for(d=0;d<n;d++)
     {
-        printf("-1");
-    }
-    else
-    {
-        int g;
-        for(g=0;g<n;g++)
+        for(i=0;i<n-1;i++)
         {
-            for(i=g+1;i<n;i++)
+            if(a[i]>a[i+1])
             {
-                if(a[i]>a[g])
-                {
-                    if(c>(a[i]-a[g]))
-                    {
-                        c = a[i]-a[g];
-                        d = a[g];
-                        e = a[i];
-                    }
-                }
-                else
-                {
-                    if(c>(a[g]-a[i]))
-                    {
-                        c = a[g]-a[i];
-                        d = a[i];
-                        e = a[g];
-                    }
-                }
+                int k;
+                k=a[i];
+                a[i]=a[i+1];
+                a[i+1]=k;
             }
         }
-        printf("%d %d",d,e);
     }
+    for(i=0;i<n-1;i++)
+    {
+        if(a[i+1]-a[i]<c)
+        {
+            c=a[i+1]-a[i];
+            j=a[i+1];
+            e=a[i];
+        }
+    }
+    printf("d %d",j,e);
 }
